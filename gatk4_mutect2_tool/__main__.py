3ef6446246873a50cfbc917b1043052b305b20b6
Shenglai <lyellooo@gmail.com>
2022-01-26 16:33:14 +0000

    BINF-608: add main func

--- gatk4_mutect2_tool/__main__.py
+++ gatk4_mutect2_tool/__main__.py
@@ -3,7 +3,9 @@ import sys
 
 from gatk4_mutect2_tool import multi_gatk4_mutect2
 
-if __name__ == "__main__":
+
+def main() -> int:
+
     # CLI Entrypoint.
     retcode = 0
 
@@ -14,5 +16,9 @@ if __name__ == "__main__":
         retcode = 1
 
     sys.exit(retcode)
+
+
+if __name__ == "__main__":
+    main()
 
 # __END__
