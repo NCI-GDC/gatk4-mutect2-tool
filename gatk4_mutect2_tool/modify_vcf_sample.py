a70362b057e2010dba2e2c5a01b790e2de4c0305
Charles Czysz <czysz@uchicago.edu>
2022-06-08 20:21:02 +0000

    Modify vcf sample util script

---
+++ gatk4_mutect2_tool/modify_vcf_sample.py
@@ -0,0 +1,88 @@
+"""
+Modify GATK4.1.2 Mutect2 VCF sample header to "TUMOR", "NORMAL"
+
+@author: Shenglai Li
+"""
+from typing import Optional
+
+import click
+import pysam
+
+tumor_bamT = str
+normal_bamT = Optional[str]
+vcfT = str
+outputT = str
+
+
+def get_sample_name(bam: str) -> str:
+    '''
+    Get sample name from BAM file
+    '''
+    sample: str
+    b = pysam.AlignmentFile(bam, 'rb')
+    sample = b.header['RG'][0]['SM']  # type: ignore
+    return sample
+
+
+def modify_vcf_sample(
+    tumor_bam: tumor_bamT, normal_bam: normal_bamT, vcf: vcfT, output: outputT
+) -> None:
+    '''
+    Modify VCF sample in the header
+    '''
+    out_vcf = output
+    reader = pysam.BGZFile(vcf, mode='rb')  # type: ignore
+    writer = pysam.BGZFile(out_vcf, mode='wb')  # type: ignore
+    try:
+        for line in reader:
+            new_line: str
+            new_line = line.decode('utf-8')
+            if new_line.startswith('#CHROM'):
+                if normal_bam:
+                    assert (
+                        normal_bam in new_line
+                    ), f'Unable to find normal sample tag in the vcf file. {normal_bam}'
+                    new_line = new_line.replace(
+                        f'{normal_bam}\t{tumor_bam}', 'NORMAL\tTUMOR'
+                    )
+                else:
+                    assert (
+                        tumor_bam in new_line
+                    ), f'Unable to find tumor sample tag in the vcf file. {tumor_bam}'
+                    new_line = new_line.replace(f'{tumor_bam}', 'TUMOR')
+                new_line = new_line + '\n'
+                writer.write(new_line.encode('utf-8'))
+            else:
+                new_line = new_line + '\n'
+                writer.write(new_line.encode('utf-8'))
+    finally:
+        writer.close()
+        reader.close()
+    pysam.tabix_index(out_vcf, preset='vcf', force=True)
+
+
+@click.command()
+@click.option('--tumor_bam', required=True, type=str)
+@click.option('--vcf', required=True)
+@click.option('--output', required=True)
+@click.option('--normal_bam', required=False)
+def main(
+    tumor_bam: tumor_bamT,
+    vcf: vcfT,
+    output: outputT,
+    normal_bam: normal_bamT = None,
+) -> None:
+    '''
+    main
+    '''
+    tumor_sample = get_sample_name(tumor_bam)
+    normal_sample = None
+    if normal_bam:
+        normal_sample = get_sample_name(normal_bam)
+    modify_vcf_sample(tumor_sample, normal_sample, vcf, output)
+
+
+if __name__ == "__main__":
+    main()
+
+# __END__
